6a3fbde85f88c3244543425e6c7cc5053f6934da
mailson <bsi.mailson@gmail.com>
2021-02-18 19:29:44 -0400

    Erros do botão excluir

--- EXERCICIO_2/novo.js
+++ EXERCICIO_2/novo.js
@@ -17,12 +17,11 @@ const novoPaciente = document.querySelector('[data-form-btn-adiciona]'); //usand
         const peso_input = objNovoPacient.peso;
         const altura_input = objNovoPacient.altura;
         const imc_input = objNovoPacient.imc;
-        // const btnApagar =  document.createElement('button');
-        // btnApagar.innerHTML = 'Apagar';
+
+
+
         
-        // console.log(btnApagar);
-        
-        const tabela = document.querySelector('[data-linha-pacient]'); //criei uma váriavel para receber o seletor do DOM da tabela Paciente.   
+        const tabela = document.querySelector('[data-linha-pacient]'); //criei uma váriavel para receber o seletor do DOM do tbody da tabela Paciente.   
 
         //Aqui criei uma varável para os elementos <td>, 
         //que conterão os dados dos atributos do novo paciente e mais tarde serão 
@@ -30,8 +29,7 @@ const novoPaciente = document.querySelector('[data-form-btn-adiciona]'); //usand
         const conteudo = `<td class="nome">${nome_input}</td>
                         <td class="imc">${imc_input}</td>
                         <td class="peso">${peso_input}</td>
-                        <td class="altura">${altura_input}</td>
-                        <td class = "delete" > <button id="delete">Apagar</button> </td>`;
+                        <td class="altura">${altura_input}</td>`;
 
         const novaLinha = document.createElement('tr'); //Aqui criei um elemento <tr> nova linha que receberá seu elmento filho
                                                         // a <td> criada anteriormente;
@@ -40,6 +38,7 @@ const novoPaciente = document.querySelector('[data-form-btn-adiciona]'); //usand
 
         novaLinha.innerHTML = conteudo; //Aqui atribuímos a variável conteúdo (que contém as <td> preenchidas com os dados)
                                         // ao html <tr> da nova linha .
+
 
         tabela.appendChild(novaLinha); //Apensei a nova linha a tabela.
         
@@ -70,16 +69,25 @@ const novoPaciente = document.querySelector('[data-form-btn-adiciona]'); //usand
         return imc.toFixed(2);
     }
 
-    const botaoDelete = document.querySelector('#delete')
+    var tbPacientes = document.querySelectorAll("table"); // var tbPacientes recebe um conjunto com todos os elementos da tabela
+
+    //console.log(pacientes);
+    tbPacientes.forEach(function(pacienteAlvo){ // forEach percorre o os elementos de tbPacientes passando cada elemento (pacienteAlvo) do array como 
+                                                //parametro para uma função anônima que escuta o evento de duplo clique do elemento paciente.
+    pacienteAlvo.addEventListener("dblclick", function(event){//evento que escuta o duplo clique e chama o método remove.
+        // console.log(event.target);
+        
+            if(event.target.tagName == 'TD'){
+                event.target.parentNode.classList.add("fadeOut"); //adiciona a classe css .fadeOut para desaparecer gradualmente da tela;
+            
+                setTimeout(function(){ //usamos a função do javascript setTimeout
+                event.target.parentNode.remove() // o event chama o target (alvo) do metodo remove para o parentNode (nó pai) ou seja, ao clicar em uma 
+                                                //td a tr inteira é que é removida.
+            },700);
+            }
                 
-    botaoDelete.addEventListener("click", function(event){//evento que escuta o clique.
-        console.log(event);
-        console.log(botaoDelete);
-        console.log(event.target.parentElement.parentElement);        
-    
-                event.target.parentElement.parentElement.remove() 
-                                
-    });	    
+        });	
+    });
 
     
         
